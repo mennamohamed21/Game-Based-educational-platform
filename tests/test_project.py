021937903316e1629d378b1d58e196551c4f1d86
Emp24 <itsjustbulletz@hotmail.com>
2019-04-29 22:03:25 +0200

    Fixing the test cases
    - Travis should pass all tests now

--- tests/test_project.py
+++ tests/test_project.py
@@ -30,14 +30,15 @@ class testGames(unittest.TestCase):
         result = self.app.get('/games/')
         #self.assertEquals(result.data , b'{"game":[{"category":"MCQ","id":1,"name":"mcq11"},'
                                        #b'{"category":"MCQ","id":2,"name":"mcq12"}]}\n')
-        self.assertIn(b'"game"' , result.data)
-        self.assertIn(b'id' , result.data)
-        self.assertIn(b'category' , result.data)
-        self.assertIn(b'name' , result.data)
+        self.assertIn(b'"game"', result.data)
+        self.assertIn(b'id', result.data)
+        self.assertIn(b'category', result.data)
+        self.assertIn(b'name', result.data)
 
     def test_ViewAGame(self):
         result = self.app.get('/game/2/mcq')
-        self.assertIn( b'"game_id":2' , result.data)
+        result.data = result.data.strip()
+        self.assertIn(b' \n      "game_id": 2', result.data)
 
 
     def test_AddNewGame(self):
@@ -57,9 +58,10 @@ class testGames(unittest.TestCase):
 
     def test_select_mcq_questions(self):
         result = self.app.get('/game/1/mcq')
+        result.data = result.data.strip()
 
-        self.assertIn(b'"game_id":1' , result.data)
-        self.assertIn(b'questions' , result.data)
+        self.assertIn(b' \n      "game_id": 1', result.data)
+        self.assertIn(b'questions', result.data)
         self.assertIn(b'"Answer1"', result.data)
         self.assertIn(b'"Answer2"', result.data)
         self.assertIn(b'"Answer3"', result.data)
@@ -69,6 +71,7 @@ class testGames(unittest.TestCase):
     def test_edit_questions(self):
 
         #testing put method
+
         sent = {"question_body": "GSDD", "Answer1": "2",
                 "Answer2": "2", "Answer3": "1","AnswerTrue": "1"}
         result = app.test_client().put(
@@ -76,12 +79,13 @@ class testGames(unittest.TestCase):
             content_type='application/json',
             data=json.dumps(sent)
         )
+        result.data = result.data.strip()
         # check result from server with expected data
         self.assertEqual(
             result.status_code,
             200
         )
-        self.assertEqual(b'{"message":"question promoted"}\n', result.data)
+        self.assertEqual(b'{\n  "message": "question promoted"\n}', result.data)
 
 
     def test_delete_game(self):
@@ -89,12 +93,13 @@ class testGames(unittest.TestCase):
             '/game/2/mcq/3/delete',
             content_type='application/json',
         )
+        result.data = result.data.strip()
         # check result from server with expected data
         self.assertEqual(
             result.status_code,
             200
         )
-        self.assertEqual(b'{"message":"question deleted "}\n', result.data)
+        self.assertEqual(b'{\n  "message": "question deleted "\n}', result.data)
 
 
 InstantiateDB()
